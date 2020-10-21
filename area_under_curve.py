d53ff8b7b76d0994f7c692007028a090f2db1ed0
Isaack Motanya <isaackmotanya6@gmail.com>
2020-10-21 10:46:28 +0300

    Update the area_under_a_curve file to have the function returnt the result and print the result outside the function Create a new py file to run on any python supported ide

---
+++ area_under_curve.py
@@ -0,0 +1,21 @@
+xmin = 0
+xmax = 10
+n = 5
+y = [4, 6, 6, 4, 4, 5]
+
+
+def trapezoidal_rule(y):
+    change_in_x = ((xmax - xmin) / n)
+    size_of_y = len(y)
+    result = 0
+    for i in range(size_of_y):
+        if i != 0 and i != size_of_y - 1:
+            result = result + 2*y[i]
+        else:
+            result = result + y[i]
+    result = result * (change_in_x / 2)
+    return result
+
+
+final_result = trapezoidal_rule(y)
+print(final_result)
